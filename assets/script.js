e8f2943197d6a6e6b865ff1d940eb8b167c430e8
HobbZ <zachobba@gmail.com>
2021-09-12 23:57:33 +1000

    updated javascript functions

--- assets/script.js
+++ assets/script.js
@@ -1,108 +1,150 @@
 
-
-//make question array
-var questions = ["What is an array", "How do you store a string to local storage", "which option below will change a html directly"];
-
+var questionsArray = ["An array can store ______________.", 
+                      "In what year was JavaScript created?", 
+                      "How do you store a string to local storage?", 
+                      "Which content insertion method is preferred for security reasons?", 
+                      "Which method do you use to generate a random whole number?", 
+                      "How do?",
+                      "",
+                      "",
+                      "",
+               
+                    ];
 //make answer array
-var answers = [];
+var answersArray = [["only strings and numbers", "strings, numbers and other arrays", "only strings",  "only numbers"],
+                    ["1995", "2003", "1989", "1981"], 
+                    ["localStorage.setItem('example-data', exampleVariable)","localStorage.getItem('example-data', exampleVariable)","localStorage.setItem(exampleVariable)","store(exampleVariable)"], 
+                    ["innerHtml","textContent"],
+                    ["Math.random(Math.floor) * array.length", "Math.random * array.length", "random() * array.length"],
+                    [],
+                    [],
+                    [],
+                    ];
 
 //list parts of page to modify here
 var timerSection = document.querySelector("#timer");
 var scoreSection = document.querySelector("#score");
-var quizContainer = document.querySelector("#quizContainer");
-var questionSection = document.querySelector("#question");
+var question = document.querySelector("#question");
 var startButton = document.querySelector("#startQuiz");
-var nextQ = document.querySelector("#nextQuestion");
-var initialInput = document.querySelector("user-initials");
-var finishButton = document.querySelector("finishQuiz");
-var msgDiv = document.querySelector("#msg");
+var nextQuestionButton = document.querySelector("#nextQuestion");
 
-var initials = localStorage.getItem("initials");
-var highScore = localStorage.getItem("highScore");
+var initialInput = document.querySelector("initials");
+var finishQuizButton = document.querySelector("finishQuiz");
+
+var formInput = document.querySelector("#formInput");
+
+//var msgDiv = document.querySelector("#msg");
+
+var answerChoices = document.querySelector("#answerChoices");
+
+var userAnswer = document.querySelector("yourAnswer");
+
+var start = document.querySelector("#start");
+
+var score = document.querySelector("#score").value;
+var initials = document.querySelector("#initials").value;
+
 var resultsContainer = document.querySelector("#resultsContainer");
+var highScoresContainer = document.querySelector("#highScoresContainer");
+
+
+index = 0;
 
 //make score variables
 var score = 0;
 
 //make timer variables
-var timer = 60;
+var timer = 180;
 
-var questionAmount = 10;
+//Start Screen with test greeting and start button shown, hide all other sections
+startButton.style.visibility = "visible";
+resultsContainer.style.visibility = "hidden";
+nextQuestionButton.style.visibility = "hidden";
+highScoresContainer.style.visibility = "hidden";
 
-function showQuestions(questions, quizContainer) {
+//start button event listener
+startButton.addEventListener("click", startQuiz);
 
+//After start button clicked hide content, button, start timer and show quiz
+function startQuiz() {
+    start.style.visibility = "hidden";
+    startButton.style.visibility =  "hidden";
+    //load first question
+    nextQuestion();
+    //nextQuestionButton.style.visibility = "visible";
+    setTimer();
 }
 
-function showResults(questions, quizContainer, resultsContainer) {
+//next question event listener
+nextQuestionButton.addEventListener("click", nextQuestion);
 
+//show a question and loop through questions when button clicked
+function nextQuestion() {
+    
+    index++;
+    if(index >= questionsArray.length) {
+        endQuiz();
+        nextQuestionButton.style.visibility = "hidden";
+    } else {
+        //Question is shown, user answers by typing letter into input box, answer is then checked, 
+        //user clicks Next to next question
+        question.textContent = "Question " + (index) + "/" + questionsArray.length + ". " + questionsArray[index];
+        answerChoices.textContent = answersArray[index];
+        userAnswer.style.visibility= "visible";
+        nextQuestionButton.style.visibility = "visible";
+        //userAnswer.textContent = "";
+        //checkAnswer();
+    }
 }
 
-showQuestions(questions, quizContainer);
+function checkAnswer() {
+    //make answer lowercase to be easier to check
+    var userAnswer = document.querySelector("yourAnswer").value.toLowerCase();
+    var possibleAnswers = answersArray[index];
 
-function displayMessage(type, message) {
+    for (let index = 0; index < possibleAnswers.length; index++) {
+        if(possibleAnswers[index].toLowerCase() == userAnswer) {
+            correctAnswer();
+    }   else {
+        incorrectAnswer();
+    }
+}
+}
+
+
+   /* if(userAnswer===questionsArray[index]) {
+        correctAnswer();
+    } else {
+        incorrectAnswer();
+    }
+
+    if(userAnswer==="") {
+        incorrectAnswer();
+    }
+
+    if (!questionsArray[index]) {
+        return;
+    }
+}*/
+
+/*function displayMessage(type, message) {
     msgDiv.textContent = message;
     msgDiv.setAttribute("class", type);
-}
+}*/
 
 function correctAnswer() {
-    questionSection.textContent = "Correct";
-    questionSection.style.color = "green";
+    quizContainer.textContent = "Correct";
+    quizContainer.style.color = "green";
     score++
     scoreSection.textContent = "Score: " + score;
 }
 
 function incorrectAnswer() {
-    questionSection.textContent = "Incorrect";
-    questionSection.style.color = "red";
+    quizContainer.textContent = "Incorrect";
+    quizContainer.style.color = "red";
     timer = timer - 5;
     timerSection.textContent = "Time left: "+ timer;
 }
-
-function nextQuestion() {
-    var questionAmount= 0;
-    var currentQuestion = questions[index];
-    for (let index = 0; index < questions.length; index++) {
-        questions[index];
-        currentQuestion = questions[index]++;
-        questionAmount++;
-        
-        questionSection.textContent = questionAmount + " / " + questions.length + "<br>" + "<hr>" + questions[index];
-    }
-    return currentQuestion;
-}
-
-function startQuiz() {
-    quizContainer.style.visibility = "visible";
-    resultsContainer.style.visibility = "hidden";
-    startButton.style.visibility =  "hidden";
-    setTimer();
-}
-
-function results(questions, quizContainer, resultsContainer) {
-    var answerContainers = quizContainer.querySelectorAll(".answers");
-
-    var userAnswer = "";
-
-    //for each question
-    for (let i = 0; i < questions.length; i++) {
-        userAnswer = (answerContainers[i].querySelector("input[name=question"));
-
-        if(userAnswer===questions[i].answer) {
-            correctAnswer();
-        } else {
-            incorrectAnswer();
-        }
-    }
-}
-
-//start button event listener
-startButton.addEventListener("click", startQuiz);
-
-//next question event listener
-nextQ.addEventListener("click", nextQuestion);
-
-//Finish quiz event listener
-
 
 //make timer function
 function setTimer() {
@@ -110,7 +152,7 @@ function setTimer() {
         timer--;
         timerSection.textContent = "Time left: "+ timer;
 
-        if (timer <= 30) {
+        if (timer <= timer/2) {
             //change timer color to orange
             timerSection.style.color = "orange";
         }
@@ -120,31 +162,56 @@ function setTimer() {
             timerSection.style.color = "red";
         }
 
+        //stop timer if quiz finished
+        if (index >= questionsArray.length) {
+            clearInterval(timerInterval);
+             timerSection.textContent = "Time: " + timer;
+            endQuiz();
+        }
+
+        //when timer reaches 0 end game and save player score
         if (timer === 0) {
-            //when timer reaches 0 end game and save player score
             clearInterval(timerInterval);
             timerSection.textContent = "Time's up";
-            endGame();
+            endQuiz();
         }
 
     }, 1000);
 }
 
-finishButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    var score = document.querySelector("#score").value;
-    var initials = document.querySelector("#initials").value;
+//finishQuizButton.addEventListener("click", storeResults);
 
-    if (initials ==="") {
-        displayMessage("Error: Initials cannot be blank");
-    } else {
-    localStorage.setItem("initials", initials);
-    localStorage.setItem("highScore", score);
-    }
-});
-
-function endGame() {
+//run function after timer or reach end or quiz questions
+function endQuiz() {
     quizContainer.style.visibility = "hidden";
     resultsContainer.style.visibility = "visible";
-    resultsContainer.textContent = "You scored " + score + "out of " + questions.length;
+    highScoresContainer.style.visibility = "hidden";
+    timerSection.textContent = "Test completed";
+    //formInput.textContent = "You scored " + score + " out of " + questionsArray.length;
+    //finishQuizButton.style.visibility = "visible";
+    storeResults();
 }
+
+function storeResults() {
+    highScoresContainer.style.visibility = "visible";
+    //resultsContainer.style.visibility = "hidden";
+    var inputInitials = document.querySelector("initials");
+    var highScore = score;
+    //if (inputInitials ==="") {
+     //   displayMessage("Error: Initials cannot be blank");
+    //} else {
+    localStorage.setItem("initials", inputInitials);
+    localStorage.setItem("highScore", highScore);
+    //}
+}
+
+function showResults() {
+    var getInitials = localStorage.getItem("initials", inputInitials);
+    var getScore = localStorage.getItem("highScore", highScore);
+    //question.textContent = getInitials + " | " + getScore;
+    return getInitials, getScore;
+}
+
+
+
+
