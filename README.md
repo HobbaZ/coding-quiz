f9cf325b5f6ad9eb26f4468f0d8cbe271575086d
HobbZ <zachobba@gmail.com>
2021-09-15 23:59:32 +1000

    made changes to css and js, almost got high score working, just needs formatting correctly

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# coding-quiz
+# Coding-Quiz
+
+I was tasked with creating a coding quiz to test users on a variety of Javascript and HTML centered questions.
+
+When you press the start button, the timer will give you 2 minutes to answer 5 questions, decrementing the time by 10 seconds each time you get a question wrong or adding to your score each time you get a question right.
+
+After you complete the quiz, you'll be asked to enter your initials and the Save Score button will save your score to the high scores page.
+
+### Screenshot
+
+
+### Technology
+JS, CSS, HTML
+
+### Site Link
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,20 @@ var questionsArray = [
 
     {
         question: "Which method do you use to generate a random whole number?",
-        answers: ["Math.random(Math.floor) * array.length", "Math.random * array.length", "random() * array.length"],
-        answer: 0,
+        answers: ["Math.random(Math.floor) % array.length", "Math.random * array.length", "Math.random(Math.floor) * array.length", "randomWholeNumber * array.length"],
+        answer: 2,
     },
 
     {
-        question: "In what year was JavaScript created?",
-        answers: ["1995", "2003", "1989", "1981"],
+        question: "What is the or operator in Javascript?",
+        answers: ["&&", "**", "||", "%"],
         answer: 2,
+    },
+
+    {
+        question: "How do you create a function in Javascript?",
+        answers: ["function myFunction{}", "function = myFunction()", "myFunction function()", "function myFunction()"],
+        answer: 3,
     },
 
     ];
@@ -42,57 +48,46 @@ var submitScoreButton = document.getElementById("endQuizButton");
 var highScoresButton = document.getElementById("highScoreButton");
 
 startQuizButton.addEventListener("click", startQuiz);
-nextQuestionButton.addEventListener("click", nextQuestion);
 submitScoreButton.addEventListener("click", submitScore);
-//highScoresButton.addEventListener("click", highScores);
+highScoresButton.addEventListener("click", displayHighScores);
 
 //answer variables
 var answer1 = document.getElementById("answer1");
 var answer2 = document.getElementById("answer2");
 var answer3 = document.getElementById("answer3");
 var answer4 = document.getElementById("answer4");
-
 var answerChoices = [answer1, answer2, answer3, answer4];
 
-//user answer function, has to be here otherwise the checkAnswer doesn't work
-for (let index = 0; index < answerChoices.length; index++) {
-    answerChoices[index].addEventListener("click", function() {
-            userAnswer = index;
+//user answer function
+for (let j = 0; j < answerChoices.length; j++) {
+    answerChoices[j].addEventListener("click", function() {
+            userAnswer = j;
             checkAnswer(userAnswer);
 })  
 }
 
 //other important variables
-//timer
 var timerSection = document.querySelector("#timer");
-
-//score
 var scoreSection = document.querySelector("#score");
-
 var questionText = document.querySelector("#question");
 var answerChoices = document.querySelector("#answerChoices");
 var userAnswer = document.querySelector("#yourAnswer");
 var score = document.querySelector("#score").value;
-
-
-index = 0;
-
-//make score variables
 var score = 0;
-
-//make timer variables
 var timer = 180;
+var currentQuestion = 0;
 
 //at start of quiz (page load)
-startQuizContainer.style.visibility = "visible";
-quizContainer.style.visibility = "hidden";
-endQuizContainer.style.visibility = "hidden";
-highScoresContainer.style.visibility = "hidden";
+startQuizContainer.style.display = "block";
+quizContainer.style.display = "none";
+endQuizContainer.style.display = "none";
+highScoresContainer.style.display = "none";
 
 //After start button clicked hide start content, start timer and show quiz content
 function startQuiz() {
-    startQuizContainer.style.visibility = "hidden";
-    quizContainer.style.visibility = "visible";
+    startQuizContainer.style.display = "none";
+    quizContainer.style.display = "block";
+    highScoresContainer.style.display = "none";
     //load first question
     nextQuestion();
     //start timer
@@ -101,58 +96,52 @@ function startQuiz() {
 
 //show a question and loop through questions when button clicked
 function nextQuestion() {
-
     //if at end of questions
-    if(index > questionsArray.length) {
-        quizContainer.style.visibility = "hidden";
-        endQuizContainer.style.visibility = "visible";
+    if(currentQuestion >= questionsArray.length) {
+        quizContainer.style.display = "none";
+        endQuizContainer.style.display = "block";
         endQuiz();
         
     } else {
-        quizContainer.style.visibility = "visible";
-        //add one to index so it displays correct number (1 instead of 0)
-        questionText.innerHTML = "<h3> Question " + (index+1) + "/" + questionsArray.length + ". </h3>" + questionsArray[index].question;
-        //answerChoices.textContent = questionsArray[index].answers[index];
-        answer1.textContent = questionsArray[index].answers[0];
-        answer2.textContent = questionsArray[index].answers[1];
-        answer3.textContent = questionsArray[index].answers[2];
-        answer4.textContent = questionsArray[index].answers[3];
-        index++;
+        quizContainer.style.display = "block";
+        //add one to currentQuestion so it displays correct number (1 instead of 0)
+        questionText.innerHTML = "<h3> Question " + (currentQuestion+1) + "/" + questionsArray.length + ". </h3>" + questionsArray[currentQuestion].question;
+
+        answer1.textContent = questionsArray[currentQuestion].answers[0];
+        answer2.textContent = questionsArray[currentQuestion].answers[1];
+        answer3.textContent = questionsArray[currentQuestion].answers[2];
+        answer4.textContent = questionsArray[currentQuestion].answers[3];
+        currentQuestion++;
     }
 }
 
 function checkAnswer(userAnswer) {
-    var correctAnswer = questionsArray[index].answer;
-
+    //put currentQuestion minus 1 otherwise answer is ahead of questions
+    var correctAnswer = questionsArray[currentQuestion-1].answer;
+    
     if (userAnswer === correctAnswer) {
-        console.log("correct answer")
         var message = document.createElement("p");
-        message.textContent = "Correct";
+        message.textContent = "Correct +1";
         message.style.color = "green";
         question.append(message);
         score++
         scoreSection.textContent = "Score: " + score;
+        
 
     }   else {
-        console.log("incorrect answer")
+        console.log("correctAnswer");
         var message = document.createElement("p");
-        message.textContent = "Incorrect";
+        message.textContent = "Incorrect, the correct answer was " + correctAnswer;
         message.style.color = "red";
-        timer = timer - 5;
+        question.append(message);
+        timer = timer - 10;
         timerSection.textContent = "Time left: "+ timer;
     }
-    nextQuestion();
+    //waits one second to go to next question
+    setTimeout(function() {
+        nextQuestion();   
+    }, 1000);
 }
-
-/*function correctAnswer() {
-    
-    nextQuestion();
-}
-
-function incorrectAnswer() {
-    
-    nextQuestion();
-}*/
 
 //make timer function
 function setTimer() {
@@ -175,7 +164,7 @@ function setTimer() {
         }
 
         //stop timer if quiz finished
-        if (index >= questionsArray.length) {
+        if (currentQuestion >= questionsArray.length) {
             clearInterval(timerInterval);
              timerSection.textContent = "Time: " + timer;
             endQuiz();
@@ -186,43 +175,57 @@ function setTimer() {
             clearInterval(timerInterval);
             endQuiz();
         }
-
-    }, 1000);
+    }, 500);
 }
 
-//run function after timer or reach end or quiz questions
+//run function after timer runs out or reach end of quiz questions
 function endQuiz() {
 if (timer === 0) {
     endQuizMessage.innerHTML = "<h1>"+"Oh no, you ran out of time!"+"</h1>"+"<br>"+"You scored " + score + " out of " + questionsArray.length+"!";
 } else {
     endQuizMessage.innerHTML = "<h1>"+"You completed the test!"+"</h1>"+"<br>"+"You scored " + score + " out of " + questionsArray.length+"!";
 }
-//send the user input and score to local storage
-//localStorage.setItem('initials', userInitials);
-//localStorage.setItem('highScore', score);
 }
 
 function submitScore(event) {
     event.preventDefault();
     var userInitials = document.querySelector("#initialsInput").value;
+    var message = document.createElement("p");
+
     if (userInitials === "") {
-        alert("Please enter your initials");
+        message.textContent = "Please enter your initials";
+        message.style.color = "red";
+        //endQuizMessage.append(message);
     } else {
         var setScore = {
             Name: userInitials, Score: score
         }
+        message.textContent = "Your score has been saved, you will now be returned to the quiz start page";
+        message.style.color = "green";
+        
+        var highScore = localStorage.getItem("highScore");
+
+        if (highScore || score > parseInt(highScore)) {
+                localStorage.setItem('highScore', JSON.stringify(setScore));
+                console.log(highScore);
+        }
+        //waits one second to reset the quiz
+        setTimeout(function() {
+            endQuizContainer.style.display = "none";
+            startQuizContainer.style.display = "block";   
+        }, 1500);
     }
-    console.log ("These are the user's " + userInitials + " and " + score);
-    localStorage.setItem('highScore', JSON.stringify(setScore));
+    endQuizMessage.append(message);
 }
 
-
-function highScores() {
+function displayHighScores() {
     //show highscores window
-    var getInitials = JSON.parse(localStorage.getItem("initials"));
-    var getScore = localStorage.getItem("highScore");
-    var highScoreText = document.querySelector("#highScoreText");
-
-    highScoresContainer.style.visibility = "visible";
-    highScoreText.textContent = getInitials + " | " + getScore;
+    if (localStorage.getItem('highScore')) {
+    var getScore = (localStorage.getItem("highScore"));
+        highScoreText.innerHTML = getScore;
+    } else {
+        var getScore = "";
+        highScoreText.innerHTML = "No high scores here yet, be the first!"
+    }
+    highScoresContainer.style.display = "block";
 }
